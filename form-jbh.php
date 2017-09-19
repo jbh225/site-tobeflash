1550596ae2a1a69ea074aa7344e202d5c75fdcdf
jbh225 <jb.hallassou@gmail.com>
2017-09-19 18:24:15 +0200

    ajout version finale formulaire modifié tobeflash

--- form-jbh.php
+++ form-jbh.php
@@ -52,69 +52,153 @@
 
                     <h2 style="text-align:center">Vous êtes intéressé par :</h2>
                     <div class="row">
-
                       <div class="col-lg-offset-3 col-lg-6 col-sm-12 col-xs-12">
+                        [select menu-230 include_blank "WEB KEY" "CLE USB" "BROCHURE VIDEO"]
+                      </div>
+                      <div class="col-lg-offset-3 col-lg-6 col-sm-12 col-xs-12">
+                        <p style="color: red;"><em>Sélectionnez le modèle de votre produit ici</em></p>
                         <select onchange="catsel(this)" class="wpcf7-form-control wpcf7-select form-control">
                           <!--<option value="-1">None</option>!-->
-                            <option value="0">---</option>
-                          <option value="1">CLE USB</option>
-                          <option value="2">WEB KEY</option>
-                          <option value="3">BROCHURE VIDEO</option>
+                          <option value="0">---</option>
+
+                          <option value="2">WEB FLYER</option>
+                          <option value="8">WEB FLYER SUR MESURE</option>
+                          <option value="3">WEB CARD</option>
+                          <option value="4">VIDEO FLYER</option>
+                          <option value="5">VIDEO CARD</option>
+                          <option value="6">VIDEO BOX</option>
+                          <option value="9">VIDEO FLYER SUR MESURE</option>
+                          <option value="1">CLE USB STANDARD</option>
+                          <option value="7">CLE USB SUR MESURE</option>
                         </select>
 
                       </div>
 
-                        <!--formulaire cle usb-->
-                        <div id="1" style="display:none">
-                          <div class="col-lg-offset-3 col-lg-6 col-sm-12 col-xs-12">
-                            [radio radio-435 default:1 "standard" "sur-mesure"]
-                            <label>REFERENCE CLE USB
-                              [select menu-341 include_blank "A-MEX" "ZEPHIR" "PEOPLE Mini" "EQUIN" "LEAD" "ALCOBA" "ASTECK" "EQUATEUR" "ICE" "CLEO" "CRYSTAL" "CLIP" "SAXO" "BLADE" "CYBER" "CODEX" "CRONO" "BANK" "PEN" "PILLS" "MAGIC" "SECRET" "PUSHY" "RUFF" "STRAP V3" "STRAP V1" "DOMINO" "RUN" "POWO" "ASWOOD" "INDICE" "MINI DYNAMIC" "DYNAMIC" "EXECUTIF" "BOOK" "IMMO" "STEEL" "SHINNY" "CLASS" "DROP" "STEP" "SPARK" "TWIST"]
-                            </label>
-                            [checkbox selectmenu-reference-dont-know "Je ne sais pas encore"]
-                          </div>
+                      <!--FORMULAIRE CLE USB STANDARD-->
+                      <div id="1" style="display:none">
+                        <div class="col-lg-offset-3 col-lg-6 col-sm-12 col-xs-12">
+                          <label>REFERENCE
+                            [select menu-341 include_blank "CLE USB - (je ne sais pas encore !)" "CLE USB - A-MEX" "CLE USB - ZEPHIR" "CLE USB -
+ PEOPLE Mini" "CLE USB - EQUIN" "CLE USB - LEAD" "CLE USB - ALCOBA" "CLE USB - ASTECK" "CLE USB - EQUATEUR" "CLE USB - ICE" "CLE USB -
+ CLEO" "CLE USB - CRYSTAL" "CLE USB - CLIP" "CLE USB - SAXO" "CLE USB - BLADE" "CLE USB - CYBER" "CLE USB - CODEX" "CLE USB - CRONO" "CLE USB - BANK" "CLE USB - PEN" "CLE USB - PILLS" "CLE USB - MAGIC" "CLE USB - SECRET" "CLE USB - PUSHY" "CLE USB - RUFF" "CLE USB - STRAP V3" "CLE USB - STRAP V1" "CLE USB - DOMINO" "CLE USB - RUN" "CLE USB - POWO" "CLE USB - ASWOOD" "CLE USB - INDICE" "CLE USB - MINI DYNAMIC" "CLE USB - DYNAMIC" "CLE USB - EXECUTIF" "CLE USB - BOOK" "CLE USB - IMMO" "CLE USB - STEEL" "CLE USB - SHINNY" "CLE USB - CLASS" "CLE USB - DROP" "CLE USB - STEP" "CLE USB - SPARK" "CLE USB - TWIST"]
+                          </label>
+
                         </div>
-                        <!--fin formulaire cle usb-->
+                      </div>
+                      <!--FIN FORMULAIRE CLE USB STANDARD-->
 
-                        <!--formulaire WEBKEY-->
-                        <div id="2" style="display:none">
-                          <div class="col-lg-offset-3 col-lg-6 col-sm-12 col-xs-12">
-                            [radio radio-797 default:1 "web card" "web flyer" "sur-mesure"]
-                            <label>FORMAT WEB FLYER
-                              [select menu-269 include_blank "A4" "A5" "Format allongé" "Format carré" "A6"]
-                            </label>
-                            [checkbox selectmenu-reference-dont-know "Je ne sais pas encore"]
-                          </div>
+                      <!--FORMULAIRE CLE USB SUR MESURE-->
+                      <div id="7" style="display:none">
+                        <div class="col-lg-offset-3 col-lg-6 col-sm-12 col-xs-12">
+                          <label>FORMAT
+                            [radio radio-528 "cle usb sur-mesure"]
+                          </label>
+
                         </div>
+                      </div>
+                      <!--FIN FORMULAIRE CLE USB SUR MESURE-->
 
-                        <!--fin formulaire WEBKEY-->
+                      <!--FORMULAIRE WEB FLYER STANDARD-->
+                      <div id="2" style="display:none">
+                        <div class="col-lg-offset-3 col-lg-6 col-sm-12 col-xs-12">
+                          <label>FORMAT
+                            [select menu-269 include_blank "WEB FLYER - (je ne sais pas encore !)" "WEB FLYER - Format A4" "WEB FLYER -
+ Format A5" "WEB FLYER - Format allongé (100X210 mm)" "WEB FLYER - Format carré (194X194 mm)" "WEB FLYER - Format A6"]
+                          </label>
 
-                        <!--formulaire VIDEO FLYER-->
-                        <div id="3" style="display:none">
-                          <div class="col-lg-offset-3 col-lg-6 col-sm-12 col-xs-12">
-                            [radio radio-195 default:1 "video card" "video flyer" "video box" "sur-mesure"]
-                            <label>FORMAT VIDEO FLYER
-                              [select menu-561 include_blank "DVD" "A5" "Format carré" "A4"]
-                            </label>
-                            [checkbox selectmenu-reference-dont-know "Je ne sais pas encore"]
-                          </div>
                         </div>
-                        <!--fin formulaire VIDEO FLYER-->
+                      </div>
 
+                      <!--FIN FORMULAIRE WEB FLYER STANDARD-->
+
+
+                      <!--FORMULAIRE WEB FLYER SUR MESURE-->
+                      <div id="8" style="display:none">
+                        <div class="col-lg-offset-3 col-lg-6 col-sm-12 col-xs-12">
+                          <label>FORMAT
+                            [radio radio-564 "web flyer sur-mesure"]
+                          </label>
+
+                        </div>
+                      </div>
+                      <!--FIN FORMULAIRE WEB FLYER SUR MESURE-->
+
+                      <!--FORMULAIRE WEBCARD-->
+                      <div id="3" style="display:none">
+                        <div class="col-lg-offset-3 col-lg-6 col-sm-12 col-xs-12">
+                          <label>FORMAT
+                            [radio radio-797 "web card - Format 55X90 mm"]
+                          </label>
+                        </div>
+                      </div>
+
+                      <!--FIN FORMULAIRE WEBCARD-->
+
+
+
+
+                      <!--FORMULAIRE VIDEO FLYER STANDARD-->
+                      <div id="4" style="display:none">
+                        <div class="col-lg-offset-3 col-lg-6 col-sm-12 col-xs-12">
+                          <label>FORMAT
+                            [select menu-561 include_blank "VIDEO FLYER - (je ne sais pas encore !)" "VIDEO FLYER - Format DVD (135X190 mm)" "VIDEO FLYER - Format A5" "VIDEO FLYER - Format carré (210X210 mm)" "VIDEO FLYER - Format A4"]
+                          </label>
+
+                        </div>
+                      </div>
+                      <!--FIN FORMULAIRE VIDEO FLYER STANDARD-->
+
+                      <!--FORMULAIRE VIDEO CARD-->
+                      <div id="5" style="display:none">
+                        <div class="col-lg-offset-3 col-lg-6 col-sm-12 col-xs-12">
+
+                          <label>FORMAT
+                            [radio radio-195 "video card - Format A10 Ecran 2.4 pouces | H:50mm L:990mm" ]
+                          </label>
+
+                        </div>
+                      </div>
+                      <!--FIN FORMULAIRE VIDEO CARD-->
+
+                      <!--formulaire VIDEO BOX-->
+                      <div id="6" style="display:none">
+                        <div class="col-lg-offset-3 col-lg-6 col-sm-12 col-xs-12">
+
+                          <label>FORMAT
+                            [radio radio-939  "video box - Format 315X230 mm"]
+                          </label>
+
+                        </div>
+                      </div>
+                      <!--fin formulaire VIDEO BOX-->
+
+                      <!--FORMULAIRE VIDEO FLYER SUR MESURE-->
+                      <div id="9" style="display:none">
+                        <div class="col-lg-offset-3 col-lg-6 col-sm-12 col-xs-12">
+                          <label>FORMAT
+                            [radio radio-793  "video flyer sur-mesure"]
+                          </label>
+
+                        </div>
+                      </div>
+                      <!--FIN FORMULAIRE VIDEO FLYER SUR MESURE-->
 
                       <div class="col-lg-offset-3 col-lg-6 col-sm-12 col-xs-12">
                         <label>QUANTITÉ SOUHAITÉE
                           [text quantity class:withcheckbox]</label>
                           [checkbox checkbox-quantity-dont-know "Je ne sais pas encore"]
 
-                            <label>VOTRE BUDGET
-                              [text budget class:withcheckbox]</label>
-                              [checkbox checkbox-budget-dont-know "Je ne sais pas encore"]
+                          <label>VOTRE BUDGET EN €
+                            [text budget class:withcheckbox]</label>
 
+                            [checkbox checkbox-budget-dont-know "Je ne sais pas encore"]
+                          </div>
+
+                          <div class="col-lg-offset-3 col-lg-6 col-sm-12 col-xs-12">
                             <label>DATE DE DÉBUT DE PROJET
                               [date date-988]</label>
-
                             </div>
+
                           </div>
                           <h2>Faites-nous part de vos premières idées !</h2>
 
@@ -134,4 +218,20 @@
                               var cat = document.getElementById(sel.value);
                               if (cat) cat.style.display="block";
                             }
+                            </script>
+
+                            <script type="text/javascript" src="http://code.jquery.com/jquery-2.1.4.min.js"></script>
+                            <script src="//cdn.jsdelivr.net/webshim/1.14.5/polyfiller.js"></script>
+                            <script>
+                            webshims.setOptions('forms-ext', {types: 'date'});
+                            webshims.polyfill('forms forms-ext');
+                            $.webshims.formcfg = {
+                              en: {
+                                dFormat: '-',
+                                dateSigns: '-',
+                                patterns: {
+                                  d: "yy-mm-dd"
+                                }
+                              }
+                            };
                             </script>
